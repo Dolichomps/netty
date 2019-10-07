3a382749b30c5a3b44aeabb0d8ed1a68bbab53e3
Jordan Tully <jtullyoops@gmail.com>
2019-10-07 12:39:16 -0300

    Update assert for 3.2. Add more error checking to lead to less crashes
    Would crash when trying to start multiple instances or trying to host multiple times. This should fix that.

--- addons/netty/core/netty.gd
+++ addons/netty/core/netty.gd
@@ -3,17 +3,22 @@ class_name Netty, "res://addons/netty/resources/netty_icon.svg"
 
 var options: NettyOptions
 var parent_node: Node
+var is_hosting: bool = false
+var is_connected_to_host: bool = false
 
 func _init(p_options: Resource, node: Node) -> void:
 	options = p_options
 	parent_node = node
 
 func start() -> void:
+	if self.is_inside_tree():
+		print("Netty: Already initialized in tree, will not reinitialize.")
+		return
 	parent_node.add_child(self)
 	add_to_group("Netty", true)
 	if get_tree().get_nodes_in_group("Netty").size() > 1:
 		push_error("Netty: More than one instance of Netty exists. There can only be one.")
-		assert false
+		assert(false)
 	var ERR
 	ERR = get_tree().connect("network_peer_connected", self, "_player_connected")
 	ERR = get_tree().connect("network_peer_disconnected", self, "_player_disconnected")
@@ -22,9 +27,9 @@ func start() -> void:
 	ERR = get_tree().connect("server_disconnected", self, "_server_disconnected")
 	if ERR != OK:
 		push_error("Netty: Failed to initilize with error code: " + str(ERR) + ". View @GlobalScope enum Error for more info.")
-		assert false
+		assert(false)
 	else:
-		print("Netty: Initialized! Using options from " + str(options.resource_path))
+		print("Netty: Initialized! Using options from " + str(options.resource_path) + ".")
 
 func _connection_success() -> void:
 	if !get_tree().is_network_server():
@@ -35,6 +40,9 @@ func _player_connected(id) -> void:
 		print("Netty Server: Peer connected!" + str(id))
 
 func shutdown() -> void:
+	if !self.is_inside_tree():
+		print("Netty: Not initialized, nothing to shutdown.")
+		return
 	get_tree().disconnect("network_peer_connected", self, "_player_connected")
 	get_tree().disconnect("network_peer_disconnected", self, "_player_disconnected")
 	get_tree().disconnect("connected_to_server", self, "_connection_success")
@@ -42,22 +50,27 @@ func shutdown() -> void:
 	get_tree().disconnect("server_disconnected", self, "_server_disconnected")
 	get_tree().set_network_peer(null)
 	print("Netty: Shutdown.")
+	is_hosting = false
 	parent_node.remove_child(self)
 
 func host_session() -> void:
+	if is_hosting == true:
+		print("Netty: Aready hosting a session, will not retry.")
+		return
 	var host: NetworkedMultiplayerENet = NetworkedMultiplayerENet.new()
 	var ERR = host.create_server(options.server_port, options.max_peers)
 	if ERR != OK:
 		push_error("Netty: Failed to start hosting session with error code: " + str(ERR) + ". View @GlobalScope enum Error for more info.")
-		assert false
-	print("Netty: Hosting a server on port: " + str(options.server_port))
+		assert(false)
+	print("Netty: Hosting a server on port: " + str(options.server_port) + ".")
 	get_tree().set_network_peer(host)
+	is_hosting = true
 
 func join_session() -> void:
 	var peer: NetworkedMultiplayerENet = NetworkedMultiplayerENet.new()
 	var ERR = peer.create_client(options.server_ip, options.server_port)
 	if ERR != OK:
 		push_error("Netty: Failed to join hosting session with error code: " + str(ERR) + ". View @GlobalScope enum Error for more info.")
-		assert false
-	print("Netty: Trying to join session with IP: " + str(options.server_ip) + " on port: " + str(options.server_port))
+		assert(false)
+	print("Netty: Trying to join session with IP: " + str(options.server_ip) + " on port: " + str(options.server_port) + ".")
 	get_tree().set_network_peer(peer)
